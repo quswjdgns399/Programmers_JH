427a5979f556b79a88dd3e339c8e7fbbe2ad7ab2
quswjdgns399 <113302464+quswjdgns399@users.noreply.github.com>
2023-05-22 19:21:21 +0900

    [Silver I] Title: 회의실 배정, Time: 524 ms, Memory: 43496 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int num = Integer.parseInt(br.readLine());
+        int arr[][] = new int[num][2];
+
+        for(int i=0; i<num; i++){
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (e1,e2)->{
+            if(e1[1] == e2[1])
+                return e1[0]-e2[0];
+            return e1[1]-e2[1];
+        });
+
+        int last_time = arr[0][1];
+        int count = 1;
+
+        for(int i=1; i<num; i++){
+            if(last_time <= arr[i][0]){
+                last_time = arr[i][1];
+                count++;
+            }
+
+        }
+
+    
+        System.out.println(count);
+    }
+}
